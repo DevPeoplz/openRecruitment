42867ad285ab8260896c26defabe183a67515c78
Luis Esteban Ramirez <luis.ramirez@devpeoplz.com>
2023-08-02 16:02:34 -0500

    test

--- src/graphql/schema/__generated__/Task/queries/index 3.ts
+++
@@ -1,7 +0,0 @@
-export { findFirstTaskQuery, findFirstTaskQueryObject } from './findFirst.base'
-export { findManyTaskQuery, findManyTaskQueryObject } from './findMany.base'
-export { countTaskQuery, countTaskQueryObject } from './count.base'
-export {
-  findUniqueTaskQuery,
-  findUniqueTaskQueryObject,
-} from './findUnique.base'
