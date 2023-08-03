a6b2b2e3965a4fd9d53be2c88245a3eb871eea9f
Luis Esteban Ramirez <luis.ramirez@devpeoplz.com>
2023-08-03 17:28:37 -0500

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,11 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { ApolloProvider } from '@apollo/client'
 import client from '@/apollo-client'
-console.log(process.env.NEXTAUTH_SECRET, process.env.NEXTAUTH_URL)
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  console.log(process.env.NEXTAUTH_SECRET, process.env.NEXTAUTH_URL)
-
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
