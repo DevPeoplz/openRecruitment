73c49701f9d585c976fdc4b602fa576f655ccc3c
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-09-07 10:58:27 -0500

    add table search

--- src/components/UI/select.tsx
+++ src/components/UI/select.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, ReactNode } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import clsx from 'clsx'
@@ -7,10 +7,14 @@ export function Select({
   selected,
   list,
   onChange,
+  defaultSize = 'w-36 sm:w-48',
+  label,
 }: {
-  selected: string
-  list: { label: string; value: string; placeholder?: boolean }[]
+  selected: string | number
+  list: { label: string; value: string | number; placeholder?: boolean }[]
   onChange?: (value: string) => void
+  defaultSize?: string
+  label?: ReactNode | string
 }) {
   const selectedElement = list.find((elm) => elm.value === selected) ?? list[0]
 
@@ -18,7 +22,12 @@ export function Select({
     <Listbox value={selected} onChange={onChange}>
       {({ open }) => (
         <>
-          <div className="relative w-36 sm:w-48">
+          {label && (
+            <Listbox.Label>
+              {typeof label === 'string' ? <span className="font-bold">{label}</span> : label}
+            </Listbox.Label>
+          )}
+          <div className={clsx('relative', defaultSize)}>
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-2 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-primary-500 sm:pr-10 sm:text-sm sm:leading-6">
               <span className="block truncate">{selectedElement?.label}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 hidden items-center pr-2 sm:flex">
@@ -33,7 +42,7 @@ export function Select({
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute z-30 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
                 {list.map((element) => (
                   <Listbox.Option
                     key={element.value}
--- src/pages/candidates/index.tsx
+++ src/pages/candidates/index.tsx
@@ -4,18 +4,35 @@ import {
   Column,
   ColumnDef,
   ColumnOrderState,
+  FilterFn,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
   Header,
+  PaginationState,
+  SortingState,
   Table,
   useReactTable,
 } from '@tanstack/react-table'
+
+import { RankingInfo, rankItem, compareItems } from '@tanstack/match-sorter-utils'
 
 import { DndProvider, useDrag, useDrop } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { makeData, Person } from './makeData'
 import clsx from 'clsx'
-import { ArrowsRightLeftIcon } from '@heroicons/react/24/outline'
+import {
+  ArrowsRightLeftIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+  MagnifyingGlassIcon,
+} from '@heroicons/react/20/solid'
+import { ArrowDownCircleIcon, ArrowUpCircleIcon } from '@heroicons/react/20/solid'
+import { Select } from '@/components/UI/select'
 
 const Page = () => {
   const sidebar = (
@@ -30,7 +47,7 @@ const Page = () => {
 
   return (
     <LayoutSideMenu sidebar={sidebar}>
-      <p>Candidate</p>
+      <h1>Candidates</h1>
       <DndProvider backend={HTML5Backend}>
         <HubTable />
       </DndProvider>
@@ -40,6 +57,25 @@ const Page = () => {
 
 Page.auth = {}
 export default Page
+
+declare module '@tanstack/table-core' {
+  interface FilterMeta {
+    itemRank: RankingInfo
+  }
+}
+
+const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value)
+
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  })
+
+  // Return if the item should be filtered in/out
+  return itemRank.passed
+}
 
 const defaultColumns: ColumnDef<Person>[] = [
   {
@@ -128,9 +164,22 @@ const DraggableColumnHeader: FC<{
       className={clsx('px-3 py-3.5 text-left text-sm font-semibold text-gray-900')}
     >
       <div ref={previewRef} className="flex flex-wrap items-center">
-        {header.isPlaceholder
-          ? null
-          : flexRender(header.column.columnDef.header, header.getContext())}
+        {header.isPlaceholder ? null : (
+          <div
+            {...{
+              className: header.column.getCanSort()
+                ? 'cursor-pointer select-none flex items-center'
+                : '',
+              onClick: header.column.getToggleSortingHandler(),
+            }}
+          >
+            {flexRender(header.column.columnDef.header, header.getContext())}
+            {{
+              asc: <ArrowUpCircleIcon className="ml-1 h-4 w-4" />,
+              desc: <ArrowDownCircleIcon className="ml-1 h-4 w-4" />,
+            }[header.column.getIsSorted() as string] ?? null}
+          </div>
+        )}
         <button ref={dragRef}>
           <ArrowsRightLeftIcon className="ml-1 h-5 w-5" />
         </button>
@@ -142,23 +191,59 @@ const DraggableColumnHeader: FC<{
 function HubTable() {
   const [data, setData] = React.useState(() => makeData(20))
   const [columns] = React.useState(() => [...defaultColumns])
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [globalFilter, setGlobalFilter] = React.useState('')
 
   const [columnOrder, setColumnOrder] = React.useState<ColumnOrderState>(
     columns.map((column) => column.id as string) //must start out with populated columnOrder so we can splice
   )
 
-  const regenerateData = () => setData(() => makeData(20))
+  const pageSizeOptions = [10, 20, 30, 50]
+
+  const [{ pageIndex, pageSize }, setPagination] = React.useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: pageSizeOptions[0],
+  })
+
+  const fetchDataOptions = {
+    pageIndex,
+    pageSize,
+  }
+
+  const pagination = React.useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  )
 
   const resetOrder = () => setColumnOrder(columns.map((column) => column.id as string))
+
+  const dataQuery = {}
+
+  const manualPagination = false
 
   const table = useReactTable({
     data,
     columns,
     state: {
+      pagination,
       columnOrder,
+      sorting,
+      globalFilter,
     },
+    onPaginationChange: setPagination,
+    manualPagination: manualPagination,
+    pageCount: dataQuery?.data?.pageCount ?? -1,
+    onSortingChange: setSorting,
     onColumnOrderChange: setColumnOrder,
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: fuzzyFilter,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    ...(!manualPagination ? { getPaginationRowModel: getPaginationRowModel() } : {}),
     debugTable: true,
     debugHeaders: true,
     debugColumns: true,
@@ -166,16 +251,20 @@ function HubTable() {
 
   return (
     <div className="w-full p-4">
-      <div className="h-4" />
-      <div className="flex flex-wrap gap-2">
-        <button onClick={() => regenerateData()} className="border p-1">
-          Regenerate
-        </button>
-        <button onClick={() => resetOrder()} className="border p-1">
+      <div className="flex flex-wrap items-center justify-between gap-1">
+        <DebouncedInput
+          value={globalFilter ?? ''}
+          onChange={(value) => setGlobalFilter(String(value))}
+          placeholder="Search all columns..."
+        />
+        <button
+          type="button"
+          onClick={() => resetOrder()}
+          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
           Reset Order
         </button>
       </div>
-      <div className="h-4" />
       <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
@@ -206,25 +295,118 @@ function HubTable() {
                     </tr>
                   ))}
                 </tbody>
-                <tfoot>
-                  {table.getFooterGroups().map((footerGroup) => (
-                    <tr key={footerGroup.id}>
-                      {footerGroup.headers.map((header) => (
-                        <th key={header.id} colSpan={header.colSpan}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(header.column.columnDef.footer, header.getContext())}
-                        </th>
-                      ))}
-                    </tr>
-                  ))}
-                </tfoot>
               </table>
             </div>
           </div>
         </div>
       </div>
-      <pre>{JSON.stringify(table.getState().columnOrder, null, 2)}</pre>
+      <div className="h-2" />
+      <div className="flex items-center justify-between gap-2">
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+          </strong>
+        </span>
+        <nav
+          className="isolate inline-flex -space-x-px rounded-md text-center shadow-sm"
+          aria-label="Pagination"
+        >
+          <button
+            className="relative inline-flex items-center rounded-l-md p-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Start</span>
+            <ChevronDoubleLeftIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+          <button
+            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Previous</span>
+            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+          <button
+            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Next</span>
+            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+          <button
+            className="relative inline-flex items-center rounded-r-md p-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">End</span>
+            <ChevronDoubleRightIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+        </nav>
+
+        <span className="flex items-center gap-1">
+          Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className="w-16 rounded border p-1"
+          />
+          <Select
+            selected={table.getState().pagination.pageSize}
+            list={pageSizeOptions.map((size) => ({ value: size, label: size.toString() }))}
+            onChange={(value: string | number) => table.setPageSize(Number(value))}
+            defaultSize="w-auto"
+            label="Page Size"
+          />
+        </span>
+      </div>
+    </div>
+  )
+}
+
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number
+  onChange: (value: string | number) => void
+  debounce?: number
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  const [value, setValue] = React.useState(initialValue)
+
+  React.useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [debounce, onChange, value])
+
+  return (
+    <div className="relative w-full min-w-[200px] rounded-md shadow-sm sm:w-4/12">
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+      </div>
+      {
+        <input
+          className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          {...props}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      }
     </div>
   )
 }
