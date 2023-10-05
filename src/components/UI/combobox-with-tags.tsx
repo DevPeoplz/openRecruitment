ecde4936664f5d044e40d7cdb6428fbcf59b48e6
Alejandro López <alejo.lpaz@devpeoplz.com>
2023-10-06 00:24:24 +0200

    Add combobox with tags component

---
+++ src/components/UI/combobox-with-tags.tsx
@@ -0,0 +1,135 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import { Combobox, Transition } from '@headlessui/react'
+import { ChevronUpDownIcon, XMarkIcon } from '@heroicons/react/20/solid'
+import clsx from 'clsx'
+
+const optionsDefault = [
+  { value: 1, label: 'Durward Reynolds' },
+  { value: 2, label: 'Kenton Towne' },
+  { value: 3, label: 'Therese Wunsch' },
+  { value: 4, label: 'Benedict Kessler' },
+  { value: 5, label: 'Katelyn Rohan' },
+]
+
+export type ComboboxWithTagsProps = {
+  comboBtnRef?: React.RefObject<HTMLButtonElement>
+  options: ({ label: string; value: string | number } & Record<string, string | number>)[]
+  placeholder?: string
+  width?: string
+  onSelectedOptionsChange?: (options: ComboboxWithTagsProps['options']) => void
+}
+
+const ComboboxWithTags: React.FC<ComboboxWithTagsProps> = ({
+  comboBtnRef,
+  options,
+  placeholder = 'Select an option...',
+  width = 'w-[250px]',
+  onSelectedOptionsChange,
+}) => {
+  options = options.length > 0 ? options : optionsDefault
+  const [selectedOptions, setSelectedOptions] = useState<ComboboxWithTagsProps['options']>([])
+  const [query, setQuery] = useState('')
+
+  const filteredOptions: typeof options =
+    query === ''
+      ? options
+      : options.filter((option: { label: string }) => {
+          return option.label
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        })
+
+  const removeSelectedOption = (option: (typeof options)[0]) => {
+    setSelectedOptions((prev) => prev.filter((opt) => opt.value !== option.value))
+  }
+
+  useEffect(() => {
+    if (onSelectedOptionsChange) {
+      onSelectedOptionsChange(selectedOptions)
+    }
+  }, [onSelectedOptionsChange, selectedOptions])
+
+  return (
+    <>
+      <Combobox
+        value={selectedOptions}
+        onChange={setSelectedOptions}
+        multiple={true}
+        className={width}
+      >
+        <div className="relative mt-1">
+          <div className="mb-1 flex flex-wrap gap-1 text-xs">
+            {selectedOptions.length > 0 && (
+              <>
+                {selectedOptions.map((option, index) => (
+                  <div key={option.value} className="flex justify-between gap-2 rounded border p-1">
+                    <p>{option.label}</p>
+                    <XMarkIcon
+                      className="h-5 w-5 text-gray-500 hover:cursor-pointer"
+                      onClick={() => removeSelectedOption(option)}
+                    />
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            {selectedOptions.length !== options.length && (
+              <Combobox.Input
+                className={clsx(
+                  width,
+                  'border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0'
+                )}
+                placeholder={placeholder}
+                onChange={(event) => setQuery(event.target.value)}
+              />
+            )}
+            <Combobox.Button
+              ref={comboBtnRef}
+              className="absolute inset-y-0 right-0 flex items-center pr-2"
+            >
+              <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {filteredOptions.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                <>
+                  {filteredOptions
+                    .filter((option) => !selectedOptions.includes(option))
+                    .map((option) => (
+                      <Combobox.Option
+                        key={option.value}
+                        className={({ active }) =>
+                          `z-10 relative cursor-default select-none py-2 px-4 ${
+                            active ? 'bg-primary-400 text-white' : 'text-gray-900'
+                          }`
+                        }
+                        value={option}
+                      >
+                        {option.label}
+                      </Combobox.Option>
+                    ))}
+                </>
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </>
+  )
+}
+
+export default ComboboxWithTags
--- src/components/views/candidate/add-candidate-view.tsx
+++ src/components/views/candidate/add-candidate-view.tsx
@@ -1,5 +1,5 @@
-import { TextField, UploadAvatar, PhoneField, UploadFile } from '@/components/UI/fields'
-import React, { useContext, useState } from 'react'
+import { PhoneField, TextField, UploadAvatar, UploadFile } from '@/components/UI/fields'
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { PlusIcon } from '@heroicons/react/20/solid'
 import { Button } from '@/components/UI/Button'
 import { useApolloClient, useMutation } from '@apollo/client'
@@ -8,12 +8,22 @@ import Alert from '@/components/alert'
 import { omit } from 'lodash'
 import Loader from '@/components/UI/loader'
 import { ModalControlContext } from '@/hooks/contexts'
+import ComboboxWithTags, { ComboboxWithTagsProps } from '@/components/UI/combobox-with-tags'
+import clsx from 'clsx'
 
 const AddCandidateView = () => {
+  const tagBtnRef = useRef<HTMLButtonElement>(null)
+  const sourceBtnRef = useRef<HTMLButtonElement>(null)
+  const jobBtnRef = useRef<HTMLButtonElement>(null)
+  const [btnVisibility, setBtnVisibility] = useState({
+    jobBtn: true,
+    tagBtn: true,
+    sourceBtn: true,
+  })
   const [_, setIsOpen] = useContext(ModalControlContext)
   const client = useApolloClient()
 
-  const [formData, setFormData] = useState<Record<string, string | null | Blob>>({
+  const [formData, setFormData] = useState<Record<string, string | string[] | null | Blob>>({
     firstName: '',
     lastName: '',
     email: '',
@@ -21,6 +31,9 @@ const AddCandidateView = () => {
     avatar: null,
     cv: null,
     coverLetter: null,
+    jobs: [],
+    tags: [],
+    sources: [],
   })
 
   const [onSubmitLoading, setOnSubmitLoading] = useState(false)
@@ -46,7 +59,7 @@ const AddCandidateView = () => {
 
           formDataToUpload.append('candidateId', res.data.createOneCandidate.id)
           files.forEach((file) => {
-            const blob = formData[file]
+            const blob = formData[file] as Blob
             if (blob) {
               formDataToUpload.append('name', file)
               formDataToUpload.append('file', blob)
@@ -107,6 +120,58 @@ const AddCandidateView = () => {
     }
   }
 
+  const handleBtnClick = (key: string) => {
+    return () => {
+      setBtnVisibility({ ...btnVisibility, [key]: false })
+    }
+  }
+
+  const handleOptionsChange = useCallback(
+    (key: string, selectedOptions: ComboboxWithTagsProps['options']) => {
+      setFormData((prev) => ({
+        ...prev,
+        [key]: selectedOptions.map((option) => option.value.toString()),
+      }))
+    },
+    [setFormData]
+  )
+
+  const handleTagOptionsChange = useCallback(
+    (selectedOptions: ComboboxWithTagsProps['options']) =>
+      handleOptionsChange('tags', selectedOptions),
+    [handleOptionsChange]
+  )
+
+  const handleSourceOptionsChange = useCallback(
+    (selectedOptions: ComboboxWithTagsProps['options']) =>
+      handleOptionsChange('sources', selectedOptions),
+    [handleOptionsChange]
+  )
+
+  const handleJobOptionsChange = useCallback(
+    (selectedOptions: ComboboxWithTagsProps['options']) =>
+      handleOptionsChange('jobs', selectedOptions),
+    [handleOptionsChange]
+  )
+
+  useEffect(() => {
+    if (!btnVisibility.tagBtn) {
+      tagBtnRef.current?.click()
+    }
+  }, [btnVisibility.tagBtn])
+
+  useEffect(() => {
+    if (!btnVisibility.sourceBtn) {
+      sourceBtnRef.current?.click()
+    }
+  }, [btnVisibility.sourceBtn])
+
+  useEffect(() => {
+    if (!btnVisibility.jobBtn) {
+      jobBtnRef.current?.click()
+    }
+  }, [btnVisibility.jobBtn])
+
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-2 p-2">
       <UploadAvatar id="avatar" onChange={handleFileInput} />
@@ -134,25 +199,63 @@ const AddCandidateView = () => {
         onChange={(e: React.FormEvent) => setFormData({ ...formData, phone: e.toString() })}
       />
       <p className="pl-2 font-bold">Jobs or Talent Pool:</p>
-      <button className="ml-2 mt-1">
+      <button
+        type="button"
+        className={clsx('ml-2 mt-1', !btnVisibility.jobBtn && 'hidden')}
+        onClick={handleBtnClick('jobBtn')}
+      >
         <span className="flex w-24 items-center gap-2 rounded border border-gray-400 p-1">
           <PlusIcon className="h-4 w-4" />
           <p>Assign</p>
         </span>
       </button>
+      {!btnVisibility.jobBtn && (
+        <ComboboxWithTags
+          options={[]}
+          comboBtnRef={jobBtnRef}
+          onSelectedOptionsChange={handleJobOptionsChange}
+          placeholder={'Select a Job or Talent Pool...'}
+        />
+      )}
       <div className="m-2 flex items-center gap-2">
-        <p>Tags:</p>
-        <button className="rounded border border-gray-400 p-1">
+        <p className={'w-16'}>Tags:</p>
+        <button
+          type="button"
+          className={clsx('rounded border border-gray-400 p-1', !btnVisibility.tagBtn && 'hidden')}
+          onClick={handleBtnClick('tagBtn')}
+        >
           <PlusIcon className="h-3 w-3" />
         </button>
+        {!btnVisibility.tagBtn && (
+          <ComboboxWithTags
+            options={[]}
+            comboBtnRef={tagBtnRef}
+            onSelectedOptionsChange={handleTagOptionsChange}
+            placeholder={'Select a Tag...'}
+          />
+        )}
       </div>
       <div className="m-2 flex items-center gap-2">
-        <p>Source:</p>
-        <button className="rounded border border-gray-400 p-1">
+        <p className={'w-16'}>Source:</p>
+        <button
+          type="button"
+          className={clsx(
+            'rounded border border-gray-400 p-1',
+            !btnVisibility.sourceBtn && 'hidden'
+          )}
+          onClick={handleBtnClick('sourceBtn')}
+        >
           <PlusIcon className="h-3 w-3" />
         </button>
+        {!btnVisibility.sourceBtn && (
+          <ComboboxWithTags
+            options={[]}
+            comboBtnRef={sourceBtnRef}
+            onSelectedOptionsChange={handleSourceOptionsChange}
+            placeholder={'Select a Source...'}
+          />
+        )}
       </div>
-
       <UploadFile label="CV or Resume:" id="cv" onChange={handleFileInput} />
       <UploadFile label="Cover Letter:" id="coverLetter" onChange={handleFileInput} />
       <div>
