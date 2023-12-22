877fe0d0ff211219301f9aa1c622c4781c0d52a0
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-12-22 09:53:45 -0500

    solve conflicts

--- src/components/views/delete-record.tsx
+++ src/components/views/delete-record.tsx
@@ -1,47 +1,28 @@
-<<<<<<< HEAD
-import { XMarkIcon } from '@heroicons/react/24/outline'
-import { Tooltip } from 'react-tooltip'
-import React, { useCallback } from 'react'
-=======
 import { TrashIcon } from '@heroicons/react/24/outline'
 import { Tooltip } from 'react-tooltip'
 import React, { useCallback, useState } from 'react'
->>>>>>> origin/develop
 import Swal from 'sweetalert2'
 import Alert from '@/components/alert'
 import { useMutation } from '@apollo/client'
 import {
   DELETE_CANDIDATE_BY_ID,
-<<<<<<< HEAD
-=======
   DELETE_EVALUATION_BY_ID,
->>>>>>> origin/develop
   DELETE_EVENT_BY_ID,
   DELETE_JOB_BY_ID,
   DELETE_TALENT_POOL_BY_ID,
 } from '@/graphql-operations/mutations'
 import { GET_HUB_CANDIDATES, GET_HUB_JOBS, GET_HUB_POOLS } from '@/graphql-operations/queries'
-<<<<<<< HEAD
-=======
 import { GET_CANDIDATE_BY_ID_QUICK_EVALUATIONS } from '@/graphql-operations/queries/dashboard-candidates'
 import Loader from '@/components/ui/loader'
->>>>>>> origin/develop
 
 type DeleteRecordProps = {
   id: number | string | null | undefined
   name?: string | null | undefined
-<<<<<<< HEAD
-  type?: 'candidate' | 'job' | 'talentPool' | 'event'
-}
-
-export const DeleteRecord: React.FC<DeleteRecordProps> = ({ id, name, type = 'candidate' }) => {
-=======
   type?: 'candidate' | 'job' | 'talentPool' | 'event' | 'evaluation'
 }
 
 export const DeleteRecord: React.FC<DeleteRecordProps> = ({ id, name, type = 'candidate' }) => {
   const [deleting, setDeleting] = useState(false)
->>>>>>> origin/develop
   const recordData = {
     candidate: {
       title: 'Candidate',
@@ -63,14 +44,11 @@ export const DeleteRecord: React.FC<DeleteRecordProps> = ({ id, name, type = 'ca
       mutation: DELETE_EVENT_BY_ID,
       refetchQueries: ['GET_HUB_EVENTS'],
     },
-<<<<<<< HEAD
-=======
     evaluation: {
       title: 'Evaluation',
       mutation: DELETE_EVALUATION_BY_ID,
       refetchQueries: [GET_CANDIDATE_BY_ID_QUICK_EVALUATIONS],
     },
->>>>>>> origin/develop
   }
 
   const [deleteRecord, { loading: deleteRecordLoading }] = useMutation(recordData[type].mutation)
@@ -108,11 +86,6 @@ export const DeleteRecord: React.FC<DeleteRecordProps> = ({ id, name, type = 'ca
     })
       .then((result) => {
         if (result.isConfirmed) {
-<<<<<<< HEAD
-          handleDelete().then(() => {
-            Alert({ message: 'Deleted successfully!', type: 'success' }).then()
-          })
-=======
           setDeleting(true)
           handleDelete()
             .then(() => {
@@ -121,7 +94,6 @@ export const DeleteRecord: React.FC<DeleteRecordProps> = ({ id, name, type = 'ca
             .finally(() => {
               setDeleting(false)
             })
->>>>>>> origin/develop
         }
       })
       .catch((error) => {
@@ -134,24 +106,16 @@ export const DeleteRecord: React.FC<DeleteRecordProps> = ({ id, name, type = 'ca
     <>
       <button
         type="button"
-<<<<<<< HEAD
-        className={'rounded border border-gray-400 bg-red-500 p-0.5 hover:shadow-inner'}
-=======
         className={'rounded bg-red-500 p-0.5 hover:bg-red-600 hover:shadow-inner'}
->>>>>>> origin/develop
         onClick={handleClickDelete}
         data-tooltip-id={`delete-${type}-${id}-tooltip`}
         data-tooltip-content={'Delete'}
       >
-<<<<<<< HEAD
-        <XMarkIcon className={'h-4 w-4 text-white'} />
-=======
         {deleting ? (
           <Loader size="h-4 w-4" fullScreen={false} />
         ) : (
           <TrashIcon className={'h-4 w-4 text-white'} />
         )}
->>>>>>> origin/develop
         <Tooltip id={`delete-${type}-${id}-tooltip`} />
       </button>
     </>
